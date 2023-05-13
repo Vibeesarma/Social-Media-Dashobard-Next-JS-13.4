a23233f0336ed2e219d2f8610cbd4a0ef07e75c9
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-05-13 07:31:24 +0100

    global screen size define

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-<main>Hello</main>
-  )
+  return <h1 className="text-red-500">Hello</h1>;
 }
