4d61546fd27dbb9e2a6d7c84b21ebcc9dc646899
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-05-16 16:40:57 +0100

    button change two status only

--- components/DarkModeButton.tsx
+++ components/DarkModeButton.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const DarkModeButton = () => {
   return (
-      <fieldset className='header__toggle toggle' aria-label='theme toggle' role='radiogroup'>
-        <div className='toggle__wrapper'>
-            <input className='toggle__input' type="radio" name='theme' id='dark' />
-            <input className='toggle__input system_input' type="radio" name='theme' id='system'/>
-            <input className='toggle__input light_input' type="radio" name='theme' id='light'  />
-            <span aria-hidden="true" className='toggle__background'></span>
-            <span aria-hidden="true" className='toggle__button'></span>
-        </div>
-        <label htmlFor='dark'>Dark</label>
-        <label htmlFor="system">System</label>
-        <label htmlFor="light" >Light</label>
+    <fieldset
+      className="header__toggle toggle"
+      aria-label="theme toggle"
+      role="radiogroup"
+    >
+      <label htmlFor="dark">
+        Dark Mode
+        <span className="visually-hidden">On</span>
+      </label>
+      <div className="toggle__wrapper">
+        <input className="toggle__input" type="radio" name="theme" id="dark" />
+        <input
+          className="toggle__input light_input"
+          type="radio"
+          name="theme"
+          id="light"
+        />
+        <span aria-hidden="true" className="toggle__background"></span>
+        <span aria-hidden="true" className="toggle__button"></span>
+      </div>
+      <label htmlFor="light" className="visually-hidden">
+        Dark Mode Off
+      </label>
     </fieldset>
-  )
-}
+  );
+};
 
-export default DarkModeButton
+export default DarkModeButton;
