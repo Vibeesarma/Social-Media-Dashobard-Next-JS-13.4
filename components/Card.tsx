3318e98b62ddbeed0c78f163ff54ee706d93af53
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-05-17 15:45:38 +0100

    some of other css fixed

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,33 +9,43 @@ const Card = ({
   username,
   arrow,
 }: Card) => {
-  const classTheme = `card--${platform}`;
+  const classTheme = `${
+    platform === "facebook"
+      ? "card__facebook"
+      : platform === "twitter"
+      ? "card__twitter"
+      : platform === "instagram"
+      ? "card__instagram"
+      : "card__youtube"
+  }`;
 
   return (
-    <div className={`card ${classTheme}`}>
-      <div className="card__platform">
-        <Image
-          className="card__icon"
-          src={icon}
-          alt={platform}
-          width={20}
-          height={20}
-        />
+    <div className={`${classTheme}`}>
+      <div className="card">
+        <div className="card__platform">
+          <Image
+            className="card__icon"
+            src={icon}
+            alt={platform}
+            width={20}
+            height={20}
+          />
 
-        <div className="card__username">{username}</div>
-      </div>
-      <div className="card__followers">
-        <div className="card__count card__count--big">{count}</div>
-        <div className="card__label">{method}</div>
-      </div>
-      <div className={`card__change card__change--${arrow}`}>
-        <Image
-          src={`/images/icon-${arrow}.svg`}
-          alt={`${arrow} arrow`}
-          width={8}
-          height={4}
-        />
-        <div className="card__number">{cardNumber}</div>
+          <div className="card__username">{username}</div>
+        </div>
+        <div className="card__followers">
+          <div className="card__count card__count--big">{count}</div>
+          <div className="card__label">{method}</div>
+        </div>
+        <div className={`card__change card__change--${arrow}`}>
+          <Image
+            src={`/images/icon-${arrow}.svg`}
+            alt={`${arrow} arrow`}
+            width={8}
+            height={4}
+          />
+          <div className="card__number">{cardNumber}</div>
+        </div>
       </div>
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="header container">
       <div className="header__title">
         <h1>Social Media Dashboard</h1>
-        <p className="text-sm font-bold dark:text-dark_text1 text-white_text1">Total Followers :23,004 Dark Mode</p>
+        <p className="text-sm font-bold dark:text-dark_text1 text-white_text1">Total Followers : 23,004 Dark Mode</p>
       </div>
       <hr className="w-full h-px my-5 bg-dark_text1 border-0 dark:bg-dark_card_hover sm:hidden"/>
       <DarkModeButton />
